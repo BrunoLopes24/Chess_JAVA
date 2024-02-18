1ce9c080d41e395e28fa211fb4a5f750614e4ff9
Bruno Lopes / <117863700+BrunoLopes24@users.noreply.github.com>
2024-02-18 14:32:27 +0000

    BoardException and defensive Programming

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -6,6 +6,9 @@ public class Board {
 	private Piece[][] pieces; // Matriz de peças [rows][columns]
 
 	public Board(int rows, int columns) {
+		if (rows < 1 || columns <1){
+			throw new BoardException("Error creating board: There must be at least 1 row and 1 column");
+		}
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns]; // O Tabuleiro vai iniciar com as peças em linha e coluna.
@@ -15,28 +18,43 @@ public class Board {
 		return rows;
 	}
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
 	public int getColumns() {
 		return columns;
 	}
 
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
-
 	public Piece piece(int row, int column){
+		if (!positionExists(row,column)){
+			throw new BoardException("Position not on the Board");
+		}
 		return pieces[row][column];
 	}
 
 	public Piece piece(Position position){
+		if (!positionExists(position)){
+			throw new BoardException("Position not on the Board");
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
 
 	public void placePiece(Piece piece, Position position){
+		if (thereisaPiece(position)){
+			throw new BoardException("There is already a piece on position: " + position);
+		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
+	}
+
+	private boolean positionExists(int row, int column){
+		return row >= 0 && row < rows && column >= 0 && column < columns;
+	}
+	public boolean positionExists(Position position){
+		return positionExists(position.getRow(), position.getColumn());
+	}
+
+	public boolean thereisaPiece(Position position){
+		if (!positionExists(position)){
+			throw new BoardException("Position not on the Board");
+		}
+		return piece(position) != null;
 	}
 }
---
+++ src/boardgame/BoardException.java
@@ -0,0 +1,8 @@
+package boardgame;
+
+public class BoardException extends RuntimeException { // Será uma excepção opcional de ser tratado
+
+	public BoardException(String message) {
+		super(message);
+	}
+}
